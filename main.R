64e3728197c2b9844152820a14b6aaec9544688a
Kryshtoson <krystof.chytry@gmail.com>
2023-03-13 18:42:19 +0100

    basic sorting of synoptic table

--- main.R
+++ main.R
@@ -2,61 +2,74 @@ library(psych)
 library(tidyverse)
 library(vegan)
 library(twinspan)
-
 data(varespec)
+`%!in%` <- Negate(`%in%`)
+
+# vegan data
 varspe <- varespec
 cl <- factor(cut(twinspan(varespec), 2))
 
-for( i in unique(cl)){
-  A <- colSums((varspe == 0)*(cl != i)) # ABSENT outside
-  B <- colSums((varspe == 0)*(cl == i)) # ABSENT IN CL
-  C <- colSums((varspe != 0)*(cl != i)) # PRESENT outside
-  D <- colSums((varspe != 0)*(cl == i)) # PRESENT IN CL
-  phi_iter <- (((A*D)-(B*C))/sqrt((A+B)*(C+D)*(A+C)*(B+D)))
+# todo phi coefficient
 
-  if(i == unique(cl)[1]){
-    phi_fin <- phi_iter
+##### This produces simple non-formated synoptic table
+syn_table <- function(varspe, cl) {
+  for (i in sort(unique(cl))) { # cluster-wise iterations
+    A <- colSums((varspe == 0) * (cl != i)) # ABSENT OUT
+    B <- colSums((varspe == 0) * (cl == i)) # ABSENT IN
+    C <- colSums((varspe != 0) * (cl != i)) # PRESENT OUT
+    D <- colSums((varspe != 0) * (cl == i)) # PRESENT IN
+    phi_iter <- (((A * D) - (B * C)) / sqrt((A + B) * (C + D) * (A + C) * (B + D)))
+
+    if (i == unique(cl)[1]) {
+      phi_fin <- phi_iter
+    }
+    else {
+      phi_fin <- cbind(phi_fin, phi_iter)
+    }
   }
-  else{
-    phi_fin <- cbind(phi_fin, phi_iter)
-  }
+
+  colnames(phi_fin) <- sort(unique(cl))
+  twintable <- mutate_if(as_tibble(rownames_to_column(data.frame(phi_fin), 'species')),
+                         is.numeric,
+                         function(x) {
+                           x = replace_na(x, 0)
+                           x[x < 0] <- 0
+                           x
+                         }
+  )
 }
+#####
 
-colnames(phi_fin) <- unique(cl)
-twintable <- mutate_if(as_tibble(rownames_to_column(data.frame(phi_fin), 'species')),
-                       is.numeric,
-                       function(x){
-                         x = replace_na(x, 0)
-                         x[x < 0] <- 0
-                         x
-                       }
-)
+# sort syn table
 th <- .2
+dx <- syn_table(varspe, cl)
+first_part <- pivot_longer(dx, -1) %>%
+  split(.$name) %>%
+  map(~.x %>% arrange(-value) %>%
+  filter(value > th)) %>%
+  bind_rows() %>%
+  select(species) %>%
+  pull()
+first_part <- first_part[!duplicated(first_part)]
+order <- c(first_part, dx$species[dx$species %!in% first_part])
 
-no_n_gr <- bind_cols(species = names(varspe), bind_cols(map(split(data.frame(varspe != 0), cl), colSums))) %>%
-  pivot_longer(-1, values_to = 'no') %>%
-  mutate(name = paste0('X', name))
-#order <-
-  twintable %>%
-  pivot_longer(cols = -c(species)) %>%
-  arrange(value < th, name, -value) %>%
-  left_join(no_n_gr) %>%
-#    mutate(value = paste0(no, ' (', round(value, 2), ')'),
-#           value = ifelse(grepl('0 \\(', value), '.', .)) %>%
-    select(species, name, value) %>%
-  pivot_wider() %>%
-    print(n = 100)
+z <- split(data.frame(varspe != 0), cl)
 
+freq <- bind_cols(
+  species = names(varspe),
+  bind_cols(split(data.frame(varspe != 0), cl) %>%
+  map(function(x) { round((colSums(x)/nrow(x))*100) }))) %>%
+  mutate(species = factor(species, levels = order)) %>%
+  arrange(species)
 
+synt <- dx %>%
+  mutate(species = factor(species, levels = order)) %>%
+  arrange(species) %>%
+  `colnames<-`(names(freq))
 
-sapply(varspe != 0, colSums)
-
-
-syn_sort <- function(syntable, threshold = .2){
-output_step <- syntable %>% pivot_longer(-1) %>% filter(abs(value) > threshold) %>% arrange(name, -abs(value)) %>%
-  distinct(species)
-left_join(output_step, syntable) %>% print(n = nrow(output_step))
+for(i in names(freq)[-1]){
+  synt[[i]] <- paste0(freq[[i]], '% (', round(synt[[i]] * 100, 0), ')')
 }
 
-
-syn_sort(twintable)
+synt %>%
+  print(n = 100)
